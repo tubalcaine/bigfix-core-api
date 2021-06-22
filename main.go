c5d2447e08667df9dc35edfa88b169e3dbb617bd
Michael Schwarz <mschwarz@multitool.net>
2021-06-22 10:26:58 -0500

    Stub working,

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 // This is not meant to be used in the package. It is just a sample main
 // that makes use of some of the bigfix module features as an example.
 
-func main() int {
+func main() {
 	fmt.Println("Demo of bigfix module")
 	fmt.Println(bigfix.Stub())
 }
